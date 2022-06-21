bfd6d9a33c6fae4eb95edf98dea9b90ac9169227
Clara Dieudonné <dieudonne.cl@gmail.com>
2022-06-21 14:02:03 +0200

    declaration of the enable/disable serice

--- sorting/src/sorting.cpp
+++ sorting/src/sorting.cpp
@@ -4,6 +4,7 @@ Sorting::Sorting() : Node("sorting_topic")
 {
     hello_sub_ = this->create_subscription<std_msgs::msg::String>("hello", 10, std::bind(&Sorting::hello_callback_, this, std::placeholders::_1));
     sorting_pub_ = this->create_publisher<std_msgs::msg::String>("sorted", 10);
+    enable_sorting_srv_ = this->create_service<sorting_msgs::srv::ToggleSorting>("enable_disable", std::bind(&Sorting::toggle_publisher_callback_, this, std::placeholders::_1, std::placeholders::_2));
 }
 
 void Sorting::hello_callback_(const std_msgs::msg::String::SharedPtr input_str)
@@ -12,6 +13,15 @@ void Sorting::hello_callback_(const std_msgs::msg::String::SharedPtr input_str)
     auto message = std_msgs::msg::String();
     std::sort(input_str->data.begin(), input_str->data.end());
     message.data = input_str->data;
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-    sorting_pub_->publish(message);
+    if (sorting_active_)
+    {
+        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+        sorting_pub_->publish(message);
+    }
+}
+
+void Sorting::toggle_publisher_callback_(const std::shared_ptr<sorting_msgs::srv::ToggleSorting::Request> request, std::shared_ptr<sorting_msgs::srv::ToggleSorting::Response> response)
+{
+    sorting_active_ = !sorting_active_;
+    response->result = true;
 }
